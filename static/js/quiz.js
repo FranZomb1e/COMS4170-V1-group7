364647f857a68045e8ca8125e2b277f85cbcae71
FranZomb1e <predator.brick@gmail.com>
2022-04-18 15:48:33 -0400

    remove redundent page during test and fix some bugs

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -1,3 +1,77 @@
+function check_answer(quiz_id, quiz_choice) {
+    $.ajax({
+        type: "POST",
+        url: "/check_answer",
+        dataType : "json",
+        contentType: "application/json; charset=utf-8",
+        data : JSON.stringify({'quiz_id': quiz_id, "quiz_choice": quiz_choice}),
+        success: function(result){
+            console.log(result);
+        },
+        error: function(request, status, error){
+            console.log("Error");
+            console.log(request);
+            console.log(status);
+            console.log(error);
+    }});
+}
+
+function check_answers(quiz_choices) {
+    $.ajax({
+        type: "POST",
+        url: "/check_answers",
+        dataType : "json",
+        contentType: "application/json; charset=utf-8",
+        data : JSON.stringify({"quiz_choices": quiz_choices}),
+        success: function(result){
+            console.log(result);
+        },
+        error: function(request, status, error){
+            console.log("Error");
+            console.log(request);
+            console.log(status);
+            console.log(error);
+    }});
+}
+
+function save_user_data(user_data) {
+    $.ajax({
+        type: "POST",
+        url: "/save_user_data",
+        dataType : "json",
+        contentType: "application/json; charset=utf-8",
+        data : JSON.stringify({"user_data": user_data}),
+        success: function(result){
+            console.log(result);
+        },
+        error: function(request, status, error){
+            console.log("Error");
+            console.log(request);
+            console.log(status);
+            console.log(error);
+    }});
+}
+
+
+function get_user_data() {
+    $.ajax({
+        type: "GET",
+        url: "/get_user_data",
+        dataType : "json",
+        contentType: "application/json; charset=utf-8",
+        data : JSON.stringify({}),
+        success: function(result){
+            console.log(result);
+        },
+        error: function(request, status, error){
+            console.log("Error");
+            console.log(request);
+            console.log(status);
+            console.log(error);
+    }});
+}
+
+
 $(document).ready(function() {
     save_user_data([], window.location.pathname)
 
@@ -26,76 +100,3 @@ $(document).ready(function() {
         check_answer($(this).attr("data-current-id"), "3")
     });
 })
-
-function check_answer(quiz_id, quiz_choice, on_success) {
-    $.ajax({
-        type: "POST",
-        url: "/check_answer",
-        dataType : "json",
-        contentType: "application/json; charset=utf-8",
-        data : JSON.stringify({'quiz_id': quiz_id, "quiz_choice": quiz_choice}),
-        success: function(result){
-            on_success(result);
-        },
-        error: function(request, status, error){
-            console.log("Error");
-            console.log(request);
-            console.log(status);
-            console.log(error);
-    }});
-}
-
-function check_answers(quiz_choices, on_success) {
-    $.ajax({
-        type: "POST",
-        url: "/check_answers",
-        dataType : "json",
-        contentType: "application/json; charset=utf-8",
-        data : JSON.stringify({"quiz_choices": quiz_choices}),
-        success: function(result){
-            on_success(result);
-        },
-        error: function(request, status, error){
-            console.log("Error");
-            console.log(request);
-            console.log(status);
-            console.log(error);
-    }});
-}
-
-function save_user_data(user_data, on_success) {
-    $.ajax({
-        type: "POST",
-        url: "/save_user_data",
-        dataType : "json",
-        contentType: "application/json; charset=utf-8",
-        data : JSON.stringify({"user_data": user_data}),
-        success: function(result){
-            on_success(result);
-        },
-        error: function(request, status, error){
-            console.log("Error");
-            console.log(request);
-            console.log(status);
-            console.log(error);
-    }});
-}
-
-
-function get_user_data(on_success) {
-    $.ajax({
-        type: "GET",
-        url: "/get_user_data",
-        dataType : "json",
-        contentType: "application/json; charset=utf-8",
-        data : JSON.stringify({}),
-        success: function(result){
-            on_success(result);
-        },
-        error: function(request, status, error){
-            console.log("Error");
-            console.log(request);
-            console.log(status);
-            console.log(error);
-    }});
-}
