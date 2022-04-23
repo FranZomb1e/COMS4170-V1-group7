974dfc31cd923b64e01c93f3b9bcb43d653e9597
FranZomb1e <predator.brick@gmail.com>
2022-04-23 13:56:34 -0400

    fix bug on quiz part [next should only be enabled after choose an answer]

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function() {
     save_current_page(window.location.pathname);
     check_quiz_from_user_data(function(result) {
@@ -12,24 +10,37 @@ $(document).ready(function() {
         window.location.href = "/quiz/1"
     });
 
-    $("#quiz-next-btn").click(function() {
-        window.location.href = "/quiz/" + (parseInt($(this).attr("data-current-id")) + 1)
-    });
-
+    
     $("#quiz-end-next-btn").click(function() {
         window.location.href = "/quiz/4"
     });
 
     $("#answer1").click(function() {
         save_quiz_choice($(this).attr("data-current-id"), "0")
+        $("#quiz-next-btn").click(function() {
+            window.location.href = "/quiz/" + (parseInt($(this).attr("data-current-id")) + 1)
+        });
+    
     });
     $("#answer2").click(function() {
         save_quiz_choice($(this).attr("data-current-id"), "1")
+        $("#quiz-next-btn").click(function() {
+            window.location.href = "/quiz/" + (parseInt($(this).attr("data-current-id")) + 1)
+        });
+    
     });
     $("#answer3").click(function() {
         save_quiz_choice($(this).attr("data-current-id"), "2")
+        $("#quiz-next-btn").click(function() {
+            window.location.href = "/quiz/" + (parseInt($(this).attr("data-current-id")) + 1)
+        });
+    
     });
     $("#answer4").click(function() {
         save_quiz_choice($(this).attr("data-current-id"), "3")
+        $("#quiz-next-btn").click(function() {
+            window.location.href = "/quiz/" + (parseInt($(this).attr("data-current-id")) + 1)
+        });
+    
     });
 })
