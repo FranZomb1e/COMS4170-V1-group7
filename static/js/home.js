233aa19c18871d1d69f77429181ed23ed9340fa8
Tian Liu <8xiaolong8@gmail.com>
2022-05-01 09:59:02 -0400

    Fixed redirect to home and save home bugs

--- static/js/home.js
+++ static/js/home.js
@@ -1,7 +1,14 @@
-
 $(document).ready(function() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
     // redirect old user to previous saved page
-    redirect_if_not_new_user();
+    if (urlParams.get('redirect') != "false") {
+        redirect_if_not_new_user();
+    }
+    
+    // save the current page that the user is on    
+    save_current_page(null)
 
     // direct user to learn intro page
     $("#home-start-btn").click(function() {
